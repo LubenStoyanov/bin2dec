6490ff3796fd4a177823f6047ad0dd9aa858992a
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2022-11-15 12:19:12 +0100

    Style button, input, typography and change error message content

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { Form, json, ActionFunction, useActionData } from "react-router-dom";
 import {
-  Flex,
-  Center,
+  Box,
   Container,
   Heading,
   Input,
   Text,
   VStack,
   Button,
+  Link,
 } from "@chakra-ui/react";
 import converter from "./bin2dec";
 import { checkFormat } from "./utils/checkformat";
 
-export type ActionData =
-  | {
-      number: null | string;
-    }
-  | undefined;
+export type ActionData = {
+  number: null | string;
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
@@ -30,27 +28,55 @@ export const action: ActionFunction = async ({ request }) => {
   const hasErrors = errors.number;
   if (hasErrors) return json<ActionData>(errors);
 
-  const result = converter(String(number));
+  const result = converter(number);
   return json<ActionData>({ number: result });
 };
 
 function App() {
   const number = useActionData() as ActionData;
-  console.log(number);
   return (
-    <Container as="main">
-      <VStack>
-        <Heading as="h1">Bin2Dec</Heading>
-        <Text>Convert binary numbers to decimals</Text>
-        <Center>
+    <Box bg="gray.200" minH="100vh">
+      <Container as="main" color="gray.700">
+        <VStack spacing={5}>
+          <Heading as="h1" size="4xl">
+            Bin2Dec
+          </Heading>
+          <Text fontSize="2xl">
+            Convert{" "}
+            <Link
+              href="https://en.wikipedia.org/wiki/Binary_number"
+              isExternal
+              color="teal.500"
+            >
+              binary numbers
+            </Link>{" "}
+            to decimals
+          </Text>
           <Form method="post">
-            <Input aria-label="convert to decimal" type="text" name="number" />
-            <Button type="submit">Convert</Button>
+            <VStack>
+              <Input
+                aria-label="convert to decimal"
+                type="text"
+                name="number"
+                autoFocus
+                borderColor="teal.500"
+                focusBorderColor="teal.500"
+                _hover={{ borderWidth: "2px" }}
+              />
+              <Button
+                type="submit"
+                bg="teal.500"
+                _hover={{ bg: "teal.400" }}
+                color="gray.200"
+              >
+                Convert
+              </Button>
+            </VStack>
           </Form>
-        </Center>
-        <Text>{number?.number}</Text>
-      </VStack>
-    </Container>
+          <Text fontSize="3xl">{number?.number}</Text>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
--- src/utils/checkformat.ts
+++ src/utils/checkformat.ts
@@ -1,13 +1,12 @@
 export const checkFormat = (string: string) => {
   if (string.length === 0) return "Input required.";
-  if (string?.length > 8) {
-    return "Input of maximal 8 digits required.";
-  }
-  if (!/^\d+$/.test(string)) return "Only digits required.";
+  if (string.length > 8) return "Input of maximal 8 digits required.";
+
+  if (!/^\d+$/.test(string)) return "Digits required.";
 
   for (let i = 0; i < string.length; i++) {
     if (parseInt(string[i]) !== 0 && parseInt(string[i]) !== 1) {
-      return "Only 0 and 1 as digits required.";
+      return "0 and 1 as digits required.";
     }
   }
 
